0b8e2652e05f117e8d5d0bf4a392d0079cd7ca54
Shatanik Mahanty <shatanikmahanty@gmail.com>
2024-06-12 22:04:27 +0530

    Refactor: Linter fix

--- lib/views/login_check.dart
+++ lib/views/login_check.dart
@@ -1,9 +1,8 @@
 //Redirection class based on login check
 import 'package:flutter/material.dart';
-import 'package:wallpaper/blocs/auth_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:wallpaper/blocs/auth_bloc.dart';
 import 'package:wallpaper/views/flutter_splash.dart';
-import 'package:wallpaper/views/NavigationPages/home/home_screen.dart';
 import 'package:wallpaper/views/navigation/bottomNavigationScaffold.dart';
 
 class LoginCheckPage extends StatelessWidget {
